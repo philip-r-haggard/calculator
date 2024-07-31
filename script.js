ac2ef1077ef5039ee0c9da550200e4c7afeddfae
Philip Haggard <philip.r.haggard@gmail.com>
2024-07-31 09:18:46 -0500

    Refactor the JS and change the UI just a bit

--- script.js
+++ script.js
@@ -1,143 +1,106 @@
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = null;
+let shouldResetScreen = false;
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const equalsButton = document.getElementById('equalsBtn');
+const clearButton = document.getElementById('clearBtn');
+const negateButton = document.getElementById('negateBtn');
+const pointButton = document.getElementById('pointBtn');
+const display = document.querySelector('.display');
+
+window.addEventListener('keydown', handleKeyboardInput);
+equalsButton.addEventListener('click', evaluate);
+clearButton.addEventListener('click', clear);
+pointButton.addEventListener('click', appendPoint);
+
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+operatorButtons.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+function appendNumber(number) {
+    if (display.textContent === '0' || shouldResetScreen) resetScreen();
+    display.textContent += number;
+}
+
+function resetScreen() {
+    display.textContent = '';
+    shouldResetScreen = false;
+}
+
+function clear() {
+    display.textContent = '0';
+    firstOperand = '';
+    secondOperand = '';
+    currentOperation = null;
+}
+
+function appendPoint() {
+    if (shouldResetScreen) resetScreen();
+    if (display.textContent === '') display.textContent = '0';
+    if (display.textContent.includes('.')) return;
+    display.textContent += '.';
+}
+
+function setOperation(operator) {
+    if (currentOperation !== null) evaluate();
+    firstOperand = display.textContent;
+    currentOperation = operator;
+    shouldResetScreen = true;
+}
+
+function evaluate() {
+    if (currentOperation === null || shouldResetScreen) return;
+    if (currentOperation === '/' && display.textContent === '0') {
+        alert("You can't divide by 0!");
+        return;
+    }
+    secondOperand = display.textContent;
+    display.textContent = roundResult(
+        operate(currentOperation, firstOperand, secondOperand)
+    );
+    currentOperation = null;
+}
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000
+}
+
+function handleKeyboardInput(e) {
+    if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+    if (e.key === '.') appendPoint();
+    if (e.key === '=' || e.key === 'Enter') evaluate();
+    if (e.key === 'Escape') clear();
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+        setOperation(e.key);
+}
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 const negate = (a) => a - (a * 2);
 
-function operate(operator, left, right) {
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
     switch (operator) {
         case '+':
-            return add(left, right);
+            return add(a, b);
         case '-':
-            return subtract(left, right);
+            return subtract(a, b);
         case '*':
-            return multiply(left, right);
+            return multiply(a, b);
         case '/':
-            return divide(left, right);
+            if (b === 0) return null;
+            else return divide(a, b);
         default:
-            return 'Woops!';
+            return null;
     }
 }
-
-function formatResult(result) {
-    let resultStr = result.toString();
-
-    if (resultStr.length > 11) {
-        if (Math.abs(result) >= 1e11 || Math.abs(result) < 1e-10) {
-            resultStr = result.toExponential(5);
-        } else {
-            let precision = 10 - Math.floor(Math.log10(Math.abs(result))) - 1;
-            resultStr = result.toFixed(precision);
-
-            if (resultStr.length > 11) {
-                resultStr = result.toExponential(6);
-            }
-        }
-    }
-
-    return resultStr;
-}
-
-function reset() {
-    operator = null;
-    left = 0;
-    right = 0;
-    finalValue = 0;
-    startOfLeftNumber = true;
-    startOfRightNumber = false;
-    workingOnLeftNumber = true;
-    display.textContent = '0';
-}
-
-function leftNumberHasStarted() {
-    startOfLeftNumber = false;
-}
-
-function hasLeftNumberStarted() {
-    if(startOfLeftNumber === false && workingOnLeftNumber === true) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function afterEqual() {
-    operator = null;
-    left = parseInt(display.textContent);
-    right = 0;
-    finalValue = 0;
-    startOfLeftNumber = false;
-    startOfRightNumber = true;
-    workingOnLeftNumber = true;
-}
-
-let operator, left, right, finalValue, startOfLeftNumber, startOfRightNumber, workingOnLeftNumber;
-
-const display = document.querySelector('.display');
-const numberButtons = document.querySelectorAll('.num');
-const operandButtons = document.querySelectorAll('.operand');
-const negateButton = document.querySelector('.negate');
-const clearButton = document.querySelector('.clear');
-const equalButton = document.querySelector('.equal');
-
-reset();
-
-numberButtons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        if (startOfLeftNumber === true) {
-            display.textContent = e.target.textContent;
-            left = parseInt(display.textContent);
-            leftNumberHasStarted();
-        } else if (hasLeftNumberStarted()) {
-            display.textContent = display.textContent + '' + e.target.textContent;
-            left = parseInt(display.textContent);
-        } else if (startOfRightNumber === true && workingOnLeftNumber === false) {
-            display.textContent = e.target.textContent;
-            right = parseInt(display.textContent);
-            startOfRightNumber = false;
-        } else if (startOfRightNumber === false && workingOnLeftNumber === false) {
-            display.textContent = display.textContent + '' + e.target.textContent;
-            right = parseInt(display.textContent);
-        }
-    });
-});
-
-operandButtons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        if (workingOnLeftNumber === true) {
-            operator = e.target.textContent;
-            workingOnLeftNumber = false;
-            startOfRightNumber = true;
-        } else if (workingOnLeftNumber === false) {
-            let tempFinalValue = operate(operator, left, right);
-            left = tempFinalValue;
-            display.textContent = left;
-            right = 0;
-            startOfRightNumber = true;
-            workingOnLeftNumber = false;
-            operator = e.target.textContent;
-        }
-    });
-});
-
-negateButton.addEventListener("click", () => {
-    if (workingOnLeftNumber === true) {
-        let tempLeftValue = negate(left);
-        left = tempLeftValue;
-        display.textContent = left;
-    } else if (workingOnLeftNumber === false) {
-        let tempRightValue = negate(right);
-        right = tempRightValue;
-        display.textContent = right;
-    }
-});
-
-equalButton.addEventListener("click", () => {
-    finalValue = operate(operator, left, right);
-    display.textContent = formatResult(finalValue);
-    afterEqual();
-});
-
-clearButton.addEventListener("click", () => {
-    reset();
-});
